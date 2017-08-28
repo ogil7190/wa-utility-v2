167da459d38663ea25cba82fd43b4a0a6594078a
ogil <androidrajpoot@gmail.com>
2017-08-29 00:27:43 +0530

    ver 1.18

--- app/src/main/java/com/bluebulls/apps/whatsapputility/activities/ChatActivity.java
+++ app/src/main/java/com/bluebulls/apps/whatsapputility/activities/ChatActivity.java
@@ -133,6 +133,7 @@ public class ChatActivity extends Activity {
                             mssgs.remove(0);
                         }
                     }
+                    
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
